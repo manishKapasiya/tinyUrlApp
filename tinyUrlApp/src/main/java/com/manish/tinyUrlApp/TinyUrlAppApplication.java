18689c8e79b4803a9d2418831f2a766303d8014e
manishKapasiya <manishkapasiya048@gmail.com>
2022-09-01 12:34:38 +0530

    Update TinyUrlAppApplication.java

--- tinyUrlApp/src/main/java/com/manish/tinyUrlApp/TinyUrlAppApplication.java
+++ tinyUrlApp/src/main/java/com/manish/tinyUrlApp/TinyUrlAppApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TinyUrlAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TinyUrlAppApplication.class, args);
+		SpringApplication.
+			run(
+			TinyUrlAppApplication.class,
+			args);
 	}
 
 }
